92e69d2182c494e248b37fce427c2a9863fcfc30
Administrator <test@test.com>
2024-12-27 11:28:10 +0800

    在Windows下无法备份

--- lib/model/Backup.dart
+++ lib/model/Backup.dart
@@ -1,14 +1,26 @@
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path/path.dart' as path;
+
+import '../l10n/l10n.dart';
 
 class BackupDate {
   late String localPath;
   final sqlfile = 'appdata.db';
   final windowsSrc = 'bin/db';
-  final AndroidPath = '/storage/emulated/0/Download/';
-  final WindowsPath = 'C:/Users/Public/Downloads/';
+  final AndroidPath = '/storage/emulated/0/Download/days';
+  final WindowsPath = 'C:/Users/Public/DaysBackup';
+//文件存在检查
+  Future<void> ensureDirectoryExists(String path) async {
+    final directory = Directory(path);
+    if (!await directory.exists()) {
+      await directory.create(recursive: true);
+    }
+  }
+
   //备份数据
   Future<void> backupDatabase(context) async {
     try {
@@ -19,16 +31,23 @@ class BackupDate {
         String path = "$databasesPath/$sqlfile";
         var database = await openDatabase(path);
         await database.close();
+        await ensureDirectoryExists(AndroidPath);
         //文件复制
-        File(path).copy('${AndroidPath}/$sqlfile');
+        await File(path).copy('${AndroidPath}/$sqlfile');
       } else {
         String executablePath = Platform.resolvedExecutable;
-        String executableDir = executablePath;
+        String executableDir = path.dirname(executablePath);
         String databasePath = '$executableDir/$windowsSrc/$sqlfile';
-        File(databasePath).copy('$WindowsPath/$sqlfile');
+        await ensureDirectoryExists(WindowsPath);
+        await File(databasePath).copy('$WindowsPath/$sqlfile');
       }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(S.of(context).BackupsS)),
+      );
     } catch (e) {
-      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("${S.of(context).BackupsF}${e.toString()}")),
+      );
     }
   }
 }
